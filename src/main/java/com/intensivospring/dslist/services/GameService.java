3682dbf3aab5b0ff36014596a26348e69ff33ce4
Gabriel Galacci <gabrielcssoad@hotmail.com>
2024-11-15 17:33:15 -0300

    Consulta SQL, Projection, Games Searching By List ID

--- src/main/java/com/intensivospring/dslist/services/GameService.java
+++ src/main/java/com/intensivospring/dslist/services/GameService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.intensivospring.dslist.dto.GameDTO;
 import com.intensivospring.dslist.dto.GameMinDTO;
 import com.intensivospring.dslist.entities.Game;
+import com.intensivospring.dslist.projections.GameMinProjection;
 import com.intensivospring.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
